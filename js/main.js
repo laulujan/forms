97a00efa63b2b5e75fed6b04f7e6da8aed6af722
Laura Lujan <lauralujanhdez@gmail.com>
2020-02-23 15:39:02 -0600

    form practice part 2

--- js/main.js
+++ js/main.js
@@ -57,3 +57,70 @@ function borrarLista(){
         miLista.classList.add("hidden");
      }
 }
+
+function validateAll(){
+    if(listError.childElementCount == 0){
+        document.getElementById("mySubmit").disabled=false;
+    }
+}
+
+function validateVehicle(){
+    var checados = true;
+    if(document.getElementById("car").checked == true || document.getElementById("bike").checked == true 
+    || document.getElementById("motorcycle").checked == true){
+        checados
+    }else{
+        createLists("Vehicle is required")
+    }
+}
+
+function showHiddenSection(str){
+    var x = document.getElementById(str);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function vehicleIsRequired(param){
+    var campo = document.getElementById(param);
+    if(campo.childNodes[3].value != ""){
+        return true
+    }else{
+        let parrafo = document.createElement("p");
+        parrafo.id="errornuevo";
+        let  parrafoContent = document.createTextNode("error campo requerido");
+        parrafo.appendChild(parrafoContent);
+        campo.appendChild(parrafo);
+    }
+  }
+  function removeVehicleError(param){
+    document.getElementById(param).removeChild(document.getElementById("errornuevo"));
+  }
+
+  function validateDoors(){
+    var campo1 = document.getElementById('doors');
+    var inputDoor = document.getElementById("car-doors").value;
+      if(isNaN(inputDoor) || inputDoor<1 || inputDoor>6){
+        let parrafo1 = document.createElement("p");
+        parrafo1.id="errornuevo";
+        let  parrafoContent1 = document.createTextNode("error ingrese un numero del 1 al 6");
+        parrafo1.appendChild(parrafoContent1);
+        campo1.appendChild(parrafo1);
+        return
+      }   
+  }
+
+ function validateAccesories(){
+    var campo2= document.getElementById('bike-accesories');
+    if(document.getElementById("basket").checked == false || document.getElementById("lantern").checked == false 
+    || document.getElementById("lock").checked == false){
+        let parrafo2 = document.createElement("p");
+        parrafo2.id="errornuevo";
+        let  parrafoContent2 = document.createTextNode("Select at least one accesory");
+        parrafo2.appendChild(parrafoContent2);
+        campo2.appendChild(parrafo2);
+        return;
+    }
+ }
